845dadfad744a565acf95d93dda2f2a84c1c8c3d
krisha kumar <krishnakumar1171998@gmail.com>
2021-09-27 20:47:20 +0530

    Initial commit

--- src/components/Movies/movies.js
+++ src/components/Movies/movies.js
@@ -2,13 +2,6 @@ import React from "react";
 import Movie from "./Movie";
 
 const Movies = ({movies}) => {
-    const settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1
-    };
 
     return (
         <div className='movies'>
